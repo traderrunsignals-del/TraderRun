106fe17a2762d2f2744d2c4d82c3fdbf6351ff6e
traderrunsignals-del <traderrunsignals@gmail.com>
2026-09-20 21:49:40 +0200

    Validate indicator license token

--- app/licencia-indicador/[token]/page.tsx
+++ app/licencia-indicador/[token]/page.tsx
@@ -13,6 +13,13 @@ export default async function LicensePage({
 }: LicensePageProps) {
   const { token } = await params
 
+  const UUID_PATTERN =
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+
+if (!UUID_PATTERN.test(token)) {
+  notFound()
+}
+
   /*
    * BUSCAR LICENCIA
    */
